8e0aab862e13fdce5b20739ca6230984a24b63d0
bobye <yelpoo@gmail.com>
2013-09-25 05:58:54 +0800

    minor modifications

--- include/d2_clustering.h
+++ include/d2_clustering.h
@@ -7,34 +7,36 @@
 extern "C" {
 #endif
 
-extern int d2_initialize(const int size_of_modalities,
-			 const int *dimensions_of_modalities
-			 );
+  /* setup problem type of D2 clustering */
+  extern int d2_initialize(const int size_of_modalities, 
+			   const int *dimensions_of_modalities 
+			   );
 
 
+  /* assign data blocks, memory are pre-allocated */
+  extern int d2_assign_data(const int size_of_samples,
+			    /** IN **/ int *size_of_supports,
+			    /** IN **/ SCALAR *data_block_supp,
+			    /** IN **/ SCALAR *data_block_w
+			    );
 
-extern int d2_assign_data(const int size_of_samples,
-			  /** IN **/ int *size_of_supports,
-			  /** IN **/ SCALAR *data_block_supp,
-			  /** IN **/ SCALAR *data_block_w
-			  );
+  /* compute the centroid of labeled sub-array, memory for outputs are pre-allocated */
+  extern int d2_compute_centroid(const int label_of_interest,
+				 /** IN     **/ int *labels,
+				 /** IN/OUT **/ int *size_of_supp, 
+				 /** OUT    **/ SCALAR *centroid_supp, 
+				 /** OUT    **/ SCALAR *centroid_w
+				 ); // centroid_supp and centroid_w should allocate enough space subject to size_of_supp
 
+  extern int d2_clustering(const int size_of_clusters,
+			   /** OUT **/ int *labels,
+			   /** OUT **/ int *size_of_supp,
+			   /** OUT **/ SCALAR *cluster_supp,
+			   /** OUT **/ SCALAR *cluster_w,
+			   /** IN  **/ int reset
+			   );
 
-extern int d2_compute_centroid(const int label_of_interest,
-			       /** IN     **/ int *labels,
-			       /** IN/OUT **/ int *size_of_supp, 
-			       /** OUT    **/ SCALAR *centroid_supp,
-			       /** OUT    **/ SCALAR *centroid_w
-			       );
-
-extern int d2_clustering(const int size_of_clusters,
-			 /** OUT **/ int *labels,
-			 /** OUT **/ int *size_of_supp,
-			 /** OUT **/ SCALAR *cluster_supp,
-			 /** OUT **/ SCALAR *cluster_w
-			 );
-
-extern int d2_free();
+  extern int d2_free();
 
 #ifdef __cplusplus
 }
--- src/d2_clustering.c
+++ src/d2_clustering.c
@@ -2,10 +2,12 @@
 #include "d2_clustering.h"
 
 
-int s_modalities, *d_modalities;
-int s_samples;
-int *s_supp;
-SCALAR **p_supp, **p_w;
+int s_modalities, // size of modalities
+  *d_modalities; // dimension of each modality
+int s_samples; // size of total samples
+int *s_supp; // size of supports, 2D array of s_modalities * s_samples in column major order
+SCALAR **p_supp, **p_w; // array of pointers of support vectors and weights, use as p_supp[s_supp[]]
+
 
 int d2_initialize(const int size_of_modalities,
 		  const int *dimensions_of_modalities) {
@@ -32,14 +34,14 @@ int d2_initialize(const int size_of_modalities,
 int d2_free() {
   s_modalities = -1;
   free(d_modalities);
-  free(p_supp); free(p_w);
+  free(p_supp); free(p_w); 
   return 0;
 }
 
 int d2_assign_data(const int size_of_samples,
-		   int *size_of_supports,
-		   SCALAR *data_block_supp,
-		   SCALAR *data_block_w
+		   /** IN **/ int *size_of_supports,
+		   /** IN **/ SCALAR *data_block_supp,
+		   /** IN **/ SCALAR *data_block_w
 		   ){
   int i,j,m;
   SCALAR *count_supp = data_block_supp;
@@ -55,15 +57,42 @@ int d2_assign_data(const int size_of_samples,
   j=0; 
   for (i=0; i<s_samples; ++i) 
     for (m=0; m<s_modalities; ++m) {
-      ++j;
       p_supp[j] = count_supp;
       p_w[j]    = count_w;
     
       count_supp += s_supp[j]* d_modalities[m];
       count_w += s_supp[j];
+      ++j;
     }
 
   return 0;
 
+}
+
+int d2_compute_centroid(const int label_of_interest,
+			/** IN     **/ int *labels,
+			/** IN/OUT **/ int *size_of_supp, 
+			/** OUT    **/ SCALAR *centroid_supp, 
+			/** OUT    **/ SCALAR *centroid_w,
+			/** IN     **/ int reset
+			){
+  // step 1: setup initial guess
+  if (reset == 1) {
+    int i, nzm =0, nzc =0;
+    SCALAR *pca_mat, *centers;
+    for (int i=0; i<s_modalities; ++i) {
+      nzc += d_modalities[i];
+      nzm += d_modalities[i] * d_modalities[i];
+    }
+    pca_mat = (SCALAR *) malloc( nzm * sizeof(SCALAR) );
+    // PCA setup
+    for (i=0; i<size_of_samples; ++i)
+      if (-1 == label_of_interest || labels[i] == label_of_interest) {
+	
+      } 
+  }
+  // step 2a: fix centroid_supp, update centroid_w
+  // step 2b: fix centroid_w, update centroid_supp
+  // step 3: stop criterion, output statistics
 }
 
