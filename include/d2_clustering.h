b4cb9988bff4a6bb25407f9c9cd80c877a816a57
Jianbo Ye <yelpoo@gmail.com>
2014-10-13 14:06:18 -0400

    real bug free

--- include/d2_clustering.h
+++ include/d2_clustering.h
@@ -7,7 +7,7 @@ extern "C" {
 
 #include "global.h"
 
-
+  // data structure to store d2 of one phase
   typedef struct {
     int dim, str, size, col;
     int *p_str;
@@ -15,6 +15,7 @@ extern "C" {
     SCALAR *p_w;  
   } sph; 
 
+  // data structure to store d2 of multiple phases
   typedef struct {
     int s_ph, size; // size of phases
     int *label;
@@ -22,11 +23,6 @@ extern "C" {
     sph *ph;
   } mph;
 
-  int d2_allocate(mph *p_data,
-		  const int size_of_phases,
-		  const int size_of_samples,
-		  const int *avg_strides,
-		  const int *dimension_of_phases);
 
   int d2_allocate_sph(sph *p_data_sph,
 		      const int d,
@@ -34,32 +30,50 @@ extern "C" {
 		      const int num,
 		      const double semicol);
 
-  int d2_load(void *fp, mph *p_data);
+  int d2_allocate(mph *p_data,
+		  const int size_of_phases,
+		  const int size_of_samples,
+		  const int *avg_strides,
+		  const int *dimension_of_phases);
 
+  int d2_load(void *fp, mph *p_data);
   int d2_free(mph *p_data);
 
+  // working variables that are visible in all algorithms
   typedef struct {
     SCALAR *C;
   } var_sph;
 
-
+  // working variables specific to Bregman ADMM
   typedef struct {
     SCALAR *X, *Z;
     SCALAR *Y;
   } var_sphBregman;
 
+  // union of working variables across multiple phases
+  typedef struct {
+    int s_ph;
+    var_sph *g_var;
+    var_sphBregman *l_var_sphBregman; // may not initialized
+  } var_mph;  
+
+
+  // interface of random centroids
+  int d2_centroid_randn(mph *p_data, 
+			int idx_ph, 
+			sph *c);
+  
+  // interface of Bregman ADMM
+  int d2_allocate_work_sphBregman(sph *ph, 
+				  var_sphBregman * var_phwork);
   int d2_centroid_sphBregman(mph *p_data, // data
+			     var_mph * var_work, // working data
 			     int idx_ph, // index of phases
 			     sph *c0,
 			     sph *c);
 
-  typedef struct {
-    int s_ph;
-    var_sph *g_var;
-    var_sphBregman *l_var_sphBregman;
-  } var_mph;  
 
-
+  // interface to users
   int d2_clustering(int k, int max_iter, mph *p_data, /** OUT **/ mph *centroids);
 
 #ifdef __cplusplus
