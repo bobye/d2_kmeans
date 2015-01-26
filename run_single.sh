db682f2cb291dd3b9020e505335f9fe4cc1b7a88
Jianbo Ye <jxy198@ist.psu.edu>
2015-01-26 15:53:45 -0500

    restructure directory

--- run_single.sh
+++ run_single.sh
@@ -5,4 +5,4 @@
 #PBS -l pmem=1gb
 cd $PBS_O_WORKDIR
 export OMP_NUM_THREADS=8
-time ./d2 -i mountaindat.txt -p 2 -n 2000 -d 3,3 -s 6,11 --phase_only 0 --clusters 10 --centroid_method 0
+time ./d2 -i data/mountaindat.d2 -p 2 -n 2000 -d 3,3 -s 6,11 --phase_only 0 --clusters 10 --centroid_method 0
