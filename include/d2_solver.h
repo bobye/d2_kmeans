7a7c19e5fc68306cda0a809e7af74e15a128cf7b
Jianbo Ye <jxy198@ist.psu.edu>
2015-01-29 20:33:47 -0500

    discard some solver interface due to inefficiency; related to #14

--- include/d2_solver.h
+++ include/d2_solver.h
@@ -5,23 +5,24 @@
 extern "C" {
 #endif
 
+#include "common.h"
 #include "blas_like.h"
 
-  void d2_solver_setup();
+  void d2_solver_setup(size_t num);
   void d2_solver_release();
   void d2_solver_debug();
-
+  /*
   double d2_match_by_coordinates(int d, int n, double *X, double *wX, int m, double *Y,  double*wY,
-				 /** OUT **/ double *x, /** OUT **/ double *lambda);
+				 __OUT__ double *x, __OUT__ double *lambda);
 
   double d2_match_by_symbols(int d, 
 			     int n, int *X, double *wX, 
 			     int m, int *Y,  double*wY, 
 			     int vocab_size, double *dist_mat,
-			     /** OUT **/ double *x, /** OUT **/ double *lambda);
-
+			     __OUT__ double *x, __OUT__ double *lambda);
+  */
   double d2_match_by_distmat(int n, int m, double *C, double *wX, double *wY, 
-			     /** OUT **/ double *x, /** OUT **/ double *lambda);
+			     /** OUT **/ double *x, /** OUT **/ double *lambda, size_t index);
 
   double d2_match_by_distmat_qp(int n, int m, double *C, double *L, double rho, double *lw, double *rw, double *x0, /** OUT **/ double *x);
   
