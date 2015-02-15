5bf7169572efec89612b2506beee546c8d8d7fa4
Jianbo Ye <jxy198@ist.psu.edu>
2015-02-14 20:58:43 -0500

    print total time

--- src/d2_clustering.c
+++ src/d2_clustering.c
@@ -445,6 +445,8 @@ int d2_clustering(int num_of_clusters,
 
   d2_free_work(&var_work);
   if (use_triangle) d2_free(&the_centroids_copy);
+
+  VPRINTF("Total time: %lf\n", nclock_end_p(&n_time));
   return 0;
 }
 
