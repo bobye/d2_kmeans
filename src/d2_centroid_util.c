b016bd6c6801a0f88faa2eed7dd0b35a70d777b0
Jianbo Ye <jxy198@ist.psu.edu>
2015-02-19 17:15:46 -0500

    mostly finish serial version of #20

--- src/d2_centroid_util.c
+++ src/d2_centroid_util.c
@@ -13,10 +13,15 @@ void calculate_distmat(sph *data_ph, int* label, size_t size, sph *c, SCALAR* C)
   int *p_supp_sym = data_ph->p_supp_sym;
   size_t *p_str_cum = data_ph->p_str_cum;
 
-  switch (c->metric_type) {
+  switch (data_ph->metric_type) {
   case D2_EUCLIDEAN_L2 :
     for (i=0;i < size;  ++i) 
       _D2_FUNC(pdist2)(dim, str, p_str[i], c->p_supp + label[i]*strxdim, p_supp + dim*p_str_cum[i], C + str*p_str_cum[i]);
+    break;
+
+  case D2_WORD_EMBED : 
+    for (i=0; i< size; ++i)
+      _D2_FUNC(pdist2_sym)(dim, str, p_str[i], c->p_supp + label[i]*strxdim, p_supp_sym + p_str_cum[i], C + str*p_str_cum[i], data_ph->vocab_vec);
     break;
 
   case D2_HISTOGRAM : 
