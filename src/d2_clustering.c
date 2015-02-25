875e315ba01c627eb21340616570acbfc8254658
Jianbo Ye <jxy198@ist.psu.edu>
2015-02-24 22:05:06 -0500

    for compatible with MPI 1.4.5

--- src/d2_clustering.c
+++ src/d2_clustering.c
@@ -336,8 +336,8 @@ size_t d2_labeling(__IN_OUT__ mph *p_data,
   }
 
 #ifdef __USE_MPI__
-  assert(sizeof(size_t)  == sizeof(uint64_t));
-  MPI_Allreduce(MPI_IN_PLACE, &count, 1, MPI_UINT64_T, MPI_SUM, MPI_COMM_WORLD);
+  assert(sizeof(size_t)  == sizeof(unsigned long long));
+  MPI_Allreduce(MPI_IN_PLACE, &count, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, MPI_COMM_WORLD);
   MPI_Allreduce(MPI_IN_PLACE, &cost,  1, MPI_DOUBLE,   MPI_SUM, MPI_COMM_WORLD);
 #endif
 
